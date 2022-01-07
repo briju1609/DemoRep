511bde85c80050bfe11d38c9f950cce25faffb5a
Nikhil Patel <prajapati.brijesh1995@gmail.com>
2022-01-06 21:54:45 -0500

    first commit using cmd prompt

---
+++ src/main/java/com/testing/TestingGit.java
@@ -0,0 +1,13 @@
+package com.testing;
+
+public class TestingGit {
+
+	
+	public void testingGit() {
+		System.out.println("Testing Git!");
+	}
+	
+	public void gitCommands() {
+		System.out.println("Git Commands!");
+	}
+}
