6d5b365cc7397ad4c4c52c361a739266fc3af916
Nikhil Patel <prajapati.brijesh1995@gmail.com>
2022-01-06 22:10:05 -0500

    update in testing-git

--- src/main/java/com/testing/TestingGit.java
+++ src/main/java/com/testing/TestingGit.java
@@ -2,12 +2,19 @@ package com.testing;
 
 public class TestingGit {
 
-	
 	public void testingGit() {
 		System.out.println("Testing Git!");
 	}
-	
+
 	public void gitCommands() {
 		System.out.println("Git Commands!");
+	}
+
+	public void car() {
+		System.out.println("car");
+	}
+
+	public void color() {
+		System.out.println("color");
 	}
 }
